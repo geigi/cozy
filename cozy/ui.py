d2626abdacbd41eb73868c78b52d5804480d54ca
geigi <github@geigi.de>
2017-07-17 18:55:12 +0200

    fixed meson build and python paths cleanup

--- cozy/ui.py
+++ cozy/ui.py
@@ -1,58 +1,29 @@
-#!/usr/bin/env python3
-
-# main.py
-#
-# Copyright (C) 2017 geigi
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program  /home/ju/GitHub/Cozyis distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import sys
-import gi
 import os
+import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio, Gdk
 
-pkgdatadir = '@DATA_DIR@'
+class CozyUI:
+  def __init__(self, pkgdatadir, app):
+    self.pkgdir = pkgdatadir
+    self.app = app
 
-class ListBoxRowWithData(Gtk.ListBoxRow):
-  def __init__(self, data):
-    super(Gtk.ListBoxRow, self).__init__()
-    self.data = data
-    label = Gtk.Label(data)
-    label.set_xalign(0.0)
-    self.add(label)
-
-class Application(Gtk.Application):
-  def __init__(self, **kwargs):
-    super().__init__(application_id='org.gnome.Audiobooks', **kwargs)
-
-  def do_activate(self):
     self.init_resources()
     self.init_css()
     self.init_window()
     self.init_actions()
 
   def init_resources(self):
-    resource = Gio.resource_load(os.path.join(pkgdatadir, 'cozy.ui.gresource'))
+    resource = Gio.resource_load(os.path.join(self.pkgdir, 'cozy.ui.gresource'))
     Gio.Resource._register(resource)
 
-    resource = Gio.resource_load(os.path.join(pkgdatadir, 'cozy.img.gresource'))
+    resource = Gio.resource_load(os.path.join(self.pkgdir, 'cozy.img.gresource'))
     Gio.Resource._register(resource)
 
     self.window_builder = Gtk.Builder.new_from_resource("/de/geigi/cozy/main_window.ui")
     self.search_builder = Gtk.Builder.new_from_resource("/de/geigi/cozy/search_popover.ui")
     self.timer_builder = Gtk.Builder.new_from_resource("/de/geigi/cozy/timer_popover.ui")
+    self.settings_builder = Gtk.Builder.new_from_resource("/de/geigi/cozy/settings.ui")
 
   def init_css(self):
     if Gtk.get_minor_version() > 18:
@@ -70,15 +41,39 @@ class Application(Gtk.Application):
 
   def init_window(self):
     window = self.window_builder.get_object("app_window")
-    window.set_application(self)
+    window.set_application(self.app)
     window.show_all()
     window.present()
 
-    # just for demo
-    scale = self.window_builder.get_object("progress_scale")
-    scale.set_range(0, 4)
     author_box = self.window_builder.get_object("author_box")
     reader_box = self.window_builder.get_object("reader_box")
+    search_button = self.window_builder.get_object("search_button")
+    search_popover = self.search_builder.get_object("search_popover")
+    timer_button = self.window_builder.get_object("timer_button")
+    timer_popover = self.timer_builder.get_object("timer_popover")
+
+    self.timer_switch = self.timer_builder.get_object("timer_switch")
+    self.timer_scale = self.timer_builder.get_object("timer_scale")
+    self.timer_spinner = self.timer_builder.get_object("timer_spinner")
+    self.timer_buffer = self.timer_builder.get_object("timer_buffer")
+
+    # init popovers
+    search_button.set_popover(search_popover)
+    timer_button.set_popover(timer_popover)
+
+    # add marks to timer scale
+    for i in range(0, 181, 15):
+      self.timer_scale.add_mark(i, Gtk.PositionType.RIGHT, None)
+
+    # timer SpinButton text format
+    self.timer_spinner.connect("value-changed", self.on_timer_changed)
+    self.init_timer_buffer()
+
+    self.app.set_app_menu(self.window_builder.get_object("app_menu"))
+
+    # DEMO #
+    scale = self.window_builder.get_object("progress_scale")
+    scale.set_range(0, 4)
 
     for i in range(1,100):
       row_a = ListBoxRowWithData(i)
@@ -91,42 +86,18 @@ class Application(Gtk.Application):
     author_box.show_all()
     reader_box.show_all()
 
-    search_button = self.window_builder.get_object("search_button")
-    popover = self.search_builder.get_object("search_popover")
-
-    search_button.set_popover(popover)
-
-    timer_button = self.window_builder.get_object("timer_button")
-    timer_popover = self.timer_builder.get_object("timer_popover")
-    timer_button.set_popover(timer_popover)
-
-    self.timer_switch = self.timer_builder.get_object("timer_switch")
-
-    self.timer_scale = self.timer_builder.get_object("timer_scale")
-
-    for i in range(0, 181, 15):
-      self.timer_scale.add_mark(i, Gtk.PositionType.RIGHT, None)
-
-    self.timer_spinner = self.timer_builder.get_object("timer_spinner")
-    self.timer_buffer = self.timer_builder.get_object("timer_buffer")
-
-    self.timer_spinner.connect("value-changed", self.on_timer_changed)
-    self.init_timer_buffer()
-
-    self.set_app_menu(self.window_builder.get_object("app_menu"))
-
   def init_actions(self):
     help_action = Gio.SimpleAction.new("help", None)
     help_action.connect("activate", self.help)
-    self.add_action(help_action)
+    self.app.add_action(help_action)
 
     about_action = Gio.SimpleAction.new("about", None)
     about_action.connect("activate", self.about)
-    self.add_action(about_action)
+    self.app.add_action(about_action)
 
     quit_action = Gio.SimpleAction.new("quit", None)
     quit_action.connect("activate", self.quit)
-    self.add_action(quit_action)
+    self.app.add_action(quit_action)
 
   def help(self, action, parameter):
     pass
@@ -157,15 +128,10 @@ class Application(Gtk.Application):
     text = str(int(value)) + " min"
     self.timer_buffer.set_text(text, len(text))
 
-def main():
-  application = Application()
-
-  try:
-    ret = application.run(sys.argv)
-  except SystemExit as e:
-    ret = e.code
-
-  sys.exit(ret)
-
-if __name__ == '__main__':
-  main()
+class ListBoxRowWithData(Gtk.ListBoxRow):
+  def __init__(self, data):
+    super(Gtk.ListBoxRow, self).__init__()
+    self.data = data
+    label = Gtk.Label(data)
+    label.set_xalign(0.0)
+    self.add(label)
