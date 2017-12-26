190440a6dd5c2b15024ea0421008a8fccd9e4099
geigi <github@geigi.de>
2017-12-26 15:01:43 +0100

    Fix reactivation of scan actions and more after db scan.

--- cozy/importer.py
+++ cozy/importer.py
@@ -95,7 +95,7 @@ def update_database(ui):
 
     Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, ui.refresh_content)
     Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, ui.switch_to_playing)
-    Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, ui.block_ui_buttons, False)
+    Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, ui.block_ui_buttons, False, True)
     Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, ui.check_for_tracks)
 
     if len(failed) > 0:
