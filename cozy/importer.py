603ef4e01fe8b843f2d95ecd3214498a7e4ee5c0
Oleg <krivorot.oleg@gmail.com>
2018-01-17 00:41:16 +0200

    Fixed working with the class artwork_cache

--- cozy/importer.py
+++ cozy/importer.py
@@ -98,7 +98,7 @@ def update_database(ui):
         if db.Track.select().where(db.Track.book == book).count() < 1:
             book.delete_instance()
 
-    db.generate_artwork_cache()
+    artwork_cache.generate_artwork_cache()
 
     Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, ui.refresh_content)
     Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, ui.switch_to_playing)
