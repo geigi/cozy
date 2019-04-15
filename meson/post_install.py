56338c4cae55e7414cae5207eee6d57188afac72
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2019-04-15 18:25:20 +0200

    This should fix post_install script issue

---
+++ meson/post_install.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from os import path, environ
+import subprocess
+
+prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+schemadir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+iconchachedir = path.join(prefix, 'share', 'icons', 'hicolor')
+
+if not environ.get('DESTDIR'):
+    print('Compiling gsettings schemas…')
+    subprocess.call(['glib-compile-schemas', schemadir])
+
+    print('Updating icon cache…')
+    subprocess.call(['gtk-update-icon-cache', iconchachedir]) 
