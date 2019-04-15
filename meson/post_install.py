3f523ea83fb6fde166f63d76daf827889973c56b
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2019-04-15 18:55:54 +0200

    Fix

--- meson/post_install.py
+++ meson/post_install.py
@@ -5,11 +5,10 @@ import subprocess
 
 prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
 schemadir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
-iconchachedir = path.join(prefix, 'share', 'icons', 'hicolor')
+datadir = path.join(prefix, 'share')
 
 if not environ.get('DESTDIR'):
     print('Compiling gsettings schemas…')
     subprocess.call(['glib-compile-schemas', schemadir])
-
     print('Updating icon cache…')
-    subprocess.call(['gtk-update-icon-cache', '-qtf', iconchachedir]) 
+    subprocess.call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
