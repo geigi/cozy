e0af610fc25eeeb764001d0d631d5a93c7983f45
Julian Geywitz <github@geigi.de>
2020-04-25 13:51:52 +0200

    Add profiler to profile single methods

---
+++ cozy/architecture/profiler.py
@@ -0,0 +1,12 @@
+import time
+
+
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
+
+        return ret
+    return wrap
