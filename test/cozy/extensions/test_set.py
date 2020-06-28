3e5dc15762777f91cbbce9c8fa23d8126fcd455a
Julian Geywitz <github@geigi.de>
2020-06-28 18:23:45 +0200

    Function to split strings in sets by a defined set of characters. This will be used for authors/readers

---
+++ test/cozy/extensions/test_set.py
@@ -0,0 +1,35 @@
+from cozy.extensions.set import split_strings_to_set
+
+
+def test_split_strings_does_nothing_for_non_seperated_element():
+    test = "This is a nice test. Nothing should be splitted."
+    result = split_strings_to_set({test})
+
+    assert {test} == result
+
+
+def test_split_strings_are_splitted():
+    test = "This/is&a,test;splitting"
+    how_it_should_be = {"This", "is", "a", "test", "splitting"}
+
+    result = split_strings_to_set({test})
+
+    assert how_it_should_be == result
+
+
+def test_split_strings_are_splitted_and_whitespace_is_removed():
+    test = "This  / is & a    ,     test ; splitting    "
+    how_it_should_be = {"This", "is", "a", "test", "splitting"}
+
+    result = split_strings_to_set({test})
+
+    assert how_it_should_be == result
+
+
+def test_split_strings_are_splitted_and_dots_are_not_splitted():
+    test = "This Dr. Prof. L. Lala / is & a    ,     test ; splitting    "
+    how_it_should_be = {"This Dr. Prof. L. Lala", "is", "a", "test", "splitting"}
+
+    result = split_strings_to_set({test})
+
+    assert how_it_should_be == result
