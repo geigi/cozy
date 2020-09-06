7d15b425c6a558ba8bc3b350dec94d16ac15bb01
Julian Geywitz <github@geigi.de>
2020-09-06 15:59:32 +0200

    WIP: new importer

--- cozy/media/importer.py
+++ cozy/media/importer.py
@@ -21,13 +21,35 @@ class Importer(EventSender):
     _fs_monitor = inject.attr(FilesystemMonitor)
     _settings = inject.attr(Settings)
 
-    def __init__(self, library: Library):
-        self._library = library
-
     def scan(self):
         self.emit_event("scan", ScanStatus.STARTED)
 
+        paths_to_scan = self._get_paths_to_scan()
 
+        for path in paths_to_scan:
+            with os.scandir(path) as it:
+                with Pool as pool:
+                    pool.map(self.import_file, it)
+
+    def _get_paths_to_scan(self) -> List[str]:
+        paths = [storage.path for storage in self._settings.storage_locations if not storage.external]
+
+        for storage in self._settings.external_storage_locations:
+            try:
+                if self._fs_monitor.is_storage_online(storage):
+                    paths.append(storage.path)
+            except StorageNotFound:
+                if os.path.exists(storage.path):
+                    paths.append(storage.path)
+
+        return paths
 
     def _get_file_count_in_dir(self, dir):
         len([name for name in os.listdir(dir) if os.path.isfile(name)])
+
+    def import_file(self, path: os.DirEntry) -> bool:
+        if not path.is_file():
+            return False
+
+        if not path.name.lower().endswith(('.mp3', '.ogg', '.flac', '.m4a', '.wav', '.opus')):
+            return False
