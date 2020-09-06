954f2441ad71985271f7ae738290c3efcd8bda02
Julian Geywitz <github@geigi.de>
2020-09-06 14:23:51 +0200

    Refactor: test inject with fixture

--- test/cozy/model/test_library.py
+++ test/cozy/model/test_library.py
@@ -1,19 +1,30 @@
+import pytest
+from peewee import SqliteDatabase
+
+from cozy.ext import inject
 from cozy.extensions.set import split_strings_to_set
 
 
-def test_library_contains_books(peewee_database):
+@pytest.fixture(autouse=True)
+def setup_inject(peewee_database):
+    inject.clear_and_configure(lambda binder: binder.bind(SqliteDatabase, peewee_database))
+    yield
+    inject.clear()
+
+
+def test_library_contains_books():
     from cozy.model.library import Library
 
-    library = Library(peewee_database)
+    library = Library()
 
     assert len(library.books) > 0
 
 
-def test_authors_contains_every_author_from_db(peewee_database):
+def test_authors_contains_every_author_from_db():
     from cozy.model.library import Library
     from cozy.db.book import Book
 
-    library = Library(peewee_database)
+    library = Library()
     books = Book.select(Book.author).distinct().order_by(Book.author)
     authors_from_db = [book.author for book in books]
     authors_from_db = list(split_strings_to_set(set(authors_from_db)))
@@ -23,11 +34,11 @@ def test_authors_contains_every_author_from_db(peewee_database):
     assert library.authors.issubset(authors_from_db)
 
 
-def test_readers_contains_every_reader_from_db(peewee_database):
+def test_readers_contains_every_reader_from_db():
     from cozy.model.library import Library
     from cozy.db.book import Book
 
-    library = Library(peewee_database)
+    library = Library()
     books = Book.select(Book.reader).distinct().order_by(Book.reader)
     readers_from_db = [book.reader for book in books]
     readers_from_db = list(split_strings_to_set(set(readers_from_db)))
--- test/cozy/model/test_settings.py
+++ test/cozy/model/test_settings.py
@@ -1,13 +1,19 @@
+import pytest
+
 import cozy.ext.inject as inject
 from peewee import SqliteDatabase
+
+
+@pytest.fixture(autouse=True)
+def setup_inject(peewee_database_storage):
+    inject.clear_and_configure(lambda binder: binder.bind(SqliteDatabase, peewee_database_storage))
+    yield
+    inject.clear()
 
 
 def test_storage_locations_contains_every_storage_location_from_db(peewee_database_storage):
     from cozy.model.settings import Settings
     from cozy.db.storage import Storage
-
-    inject.clear_and_configure(lambda binder: binder
-                               .bind(SqliteDatabase, peewee_database_storage))
 
     settings = Settings()
     storage_locations = Storage.select()
@@ -17,5 +23,3 @@ def test_storage_locations_contains_every_storage_location_from_db(peewee_databa
     assert [storage.location_type for storage in settings.storage_locations] == [storage.location_type for storage in storage_locations]
     assert [storage.default for storage in settings.storage_locations] == [storage.default for storage in storage_locations]
     assert [storage.external for storage in settings.storage_locations] == [storage.external for storage in storage_locations]
-
-    inject.clear()
