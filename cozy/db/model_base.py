a44b27dcac894cdb6d5109ad7db874c548f15c12
geigi <github@geigi.de>
2020-09-27 08:48:33 +0200

    Fix: increase database busy timeout

--- cozy/db/model_base.py
+++ cozy/db/model_base.py
@@ -24,7 +24,7 @@ def __open_database():
     global _db
     if not os.path.exists(get_data_dir()):
         os.makedirs(get_data_dir())
-    _db = APSWDatabase(os.path.join(get_data_dir(), "cozy.db"), pragmas=[('journal_mode', 'wal')])
+    _db = APSWDatabase(os.path.join(get_data_dir(), "cozy.db"), timeout=5, pragmas=[('journal_mode', 'wal')])
 
 
 __open_database()
