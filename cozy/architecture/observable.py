ce699ca47d513138f52ab20c8974383cb228692a
geigi <github@geigi.de>
2020-11-15 14:54:40 +0100

    Expand observable and fix exception when a prop is not registered yet

--- cozy/architecture/observable.py
+++ cozy/architecture/observable.py
@@ -16,10 +16,33 @@ class Observable:
         else:
             self._observers[prop] = [callback]
 
+    def remove_bind(self, prop: str, callback: Callable):
+        if not prop:
+            log.error("Cannot remove bind for empty prop.")
+            reporter.error("observable", "Cannot remove bind for empty prop.")
+            return
+
+        if not callback:
+            log.error("Cannot remove bind for empty callback.")
+            reporter.error("observable", "Cannot remove bind for empty callback.")
+            return
+
+        if prop in self._observers:
+            if callback in self._observers[prop]:
+                self._observers[prop].remove(callback)
+            else:
+                log.info("Callback not found in prop's {} observers. Skipping remove bind...".format(prop))
+        else:
+            log.info("Prop not found in observers. Skipping remove bind...")
+
     def _notify(self, prop: str):
         try:
-            for callback in self._observers[prop]:
-                callback()
+            if prop in self._observers:
+                for callback in self._observers[prop]:
+                    callback()
         except Exception as e:
             log.error(e)
             reporter.exception("observable", e)
+
+    def _destroy_observers(self):
+        self._observers = {}
