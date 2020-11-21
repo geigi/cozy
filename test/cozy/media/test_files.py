1c3e490229bbd14c237727799dcd486798cc3561
Julian Geywitz <github@geigi.de>
2020-11-21 16:35:39 +0100

    Fix: possible null exception in update copy progress method

---
+++ test/cozy/media/test_files.py
@@ -0,0 +1,22 @@
+def test_update_copy_status_emits_1_for_zero_files(mocker):
+    from cozy.media.files import Files
+
+    files = Files()
+    spy = mocker.spy(files, "emit_event_main_thread")
+
+    files._file_count = 0
+    files._update_copy_status(1, 2, None)
+    spy.assert_called_once_with("copy-progress", 1.0)
+
+
+def test_update_copy_status_emits_1_for_zero_byte_files(mocker):
+    from cozy.media.files import Files
+
+    files = Files()
+    spy = mocker.spy(files, "emit_event_main_thread")
+
+    files._file_count = 1
+    files._file_progess = 1
+    files._update_copy_status(0, 0, None)
+
+    spy.assert_called_once_with("copy-progress", 0.0)
