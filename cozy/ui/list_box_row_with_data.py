29653193a7f9e9d06410026d86b7e3b07d1b0dae
Julian Geywitz <git@geigi.de>
2021-06-19 18:20:56 +0200

    Author/Reader List: Visual overhaul

--- cozy/ui/list_box_row_with_data.py
+++ cozy/ui/list_box_row_with_data.py
@@ -9,16 +9,25 @@ class ListBoxRowWithData(Gtk.ListBoxRow):
     """
     This class represents a listboxitem for an author/reader.
     """
-    MARGIN = 10
+    LABEL_MARGIN = 8
+    ROW_MARGIN = 4
 
-    def __init__(self, data, bold=False):
-        super(Gtk.ListBoxRow, self).__init__()
+    def __init__(self, data, bold=False, **properties):
+        super().__init__(**properties)
         self.data = data
+
+        self.set_margin_top(self.ROW_MARGIN)
+        self.set_margin_bottom(self.ROW_MARGIN)
+        self.set_margin_start(self.ROW_MARGIN)
+        self.set_margin_end(self.ROW_MARGIN)
+
+        self.get_style_context().add_class("filter-list-box-row")
+
         label = Gtk.Label.new(data)
         if bold:
             label.set_markup("<b>" + data + "</b>")
         label.set_xalign(0.0)
-        label.set_margin_top(self.MARGIN)
-        label.set_margin_bottom(self.MARGIN)
+        label.set_margin_top(self.LABEL_MARGIN)
+        label.set_margin_bottom(self.LABEL_MARGIN)
         label.set_margin_start(7)
         self.add(label)
