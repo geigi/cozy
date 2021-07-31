e07ea50deb1520462130c1d94123979c8068d704
Julian Geywitz <git@geigi.de>
2021-07-31 09:09:24 +0200

    Very long author or reader names get ellipsized in the library view

--- cozy/ui/list_box_row_with_data.py
+++ cozy/ui/list_box_row_with_data.py
@@ -1,8 +1,4 @@
-from typing import List
-
-from gi.repository import Gtk
-
-from cozy.ui.list_box_separator_row import ListBoxSeparatorRow
+from gi.repository import Gtk, Pango
 
 
 class ListBoxRowWithData(Gtk.ListBoxRow):
@@ -23,11 +19,14 @@ class ListBoxRowWithData(Gtk.ListBoxRow):
 
         self.get_style_context().add_class("filter-list-box-row")
 
-        label = Gtk.Label.new(data)
+        label: Gtk.Label = Gtk.Label.new(data)
         if bold:
             label.set_markup("<b>" + data + "</b>")
         label.set_xalign(0.0)
         label.set_margin_top(self.LABEL_MARGIN)
         label.set_margin_bottom(self.LABEL_MARGIN)
         label.set_margin_start(7)
+        label.set_max_width_chars(30)
+        label.set_ellipsize(Pango.EllipsizeMode.END)
         self.add(label)
+        self.set_tooltip_text(data)
