0fd3744947fa822757387562b33934fae69d88f4
Julian Geywitz <git@geigi.de>
2021-10-24 16:02:09 +0200

    Replace sleep timer icon
    Issue: #652

--- cozy/ui/widgets/sleep_timer.py
+++ cozy/ui/widgets/sleep_timer.py
@@ -46,7 +46,6 @@ class SleepTimer(Gtk.Popover):
     def _connect_view_model(self):
         self._view_model.bind_to("stop_after_chapter", self._on_stop_after_chapter_changed)
         self._view_model.bind_to("remaining_seconds", self._on_remaining_seconds_changed)
-        self._view_model.bind_to("timer_enabled", self._on_timer_enabled_changed)
 
     def _init_timer_scale(self):
         for i in range(0, 121, 30):
@@ -94,11 +93,3 @@ class SleepTimer(Gtk.Popover):
 
     def _on_stop_after_chapter_changed(self):
         self.chapter_switch.set_active(self._view_model.stop_after_chapter)
-
-    def _on_timer_enabled_changed(self):
-        if self._view_model.timer_enabled:
-            icon = "timer-on-symbolic"
-        else:
-            icon = "timer-off-symbolic"
-
-        self._timer_image.set_from_icon_name(icon, Gtk.IconSize.BUTTON)
