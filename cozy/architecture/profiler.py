ae1724c5e13e9378d5db2e34fe193b3fe52686a5
Julian Geywitz <git@geigi.de>
2022-04-17 16:10:02 +0200

    print profiling to debug log

--- cozy/architecture/profiler.py
+++ cozy/architecture/profiler.py
@@ -1,12 +1,14 @@
 import time
+import logging
 
+log = logging.getLogger("timing")
 
 def timing(f):
     def wrap(*args):
         time1 = time.time()
         ret = f(*args)
         time2 = time.time()
-        print('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
+        log.debug('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
 
         return ret
     return wrap
