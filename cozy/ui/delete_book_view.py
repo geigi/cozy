fed89df230bd4b872e1cda34ecc284f0c7f6594f
rdbende <rdbende@proton.me>
2023-11-14 22:16:22 +0100

    Then all this is unnecessary

--- cozy/ui/delete_book_view.py
+++ cozy/ui/delete_book_view.py
@@ -2,17 +2,22 @@ import gi
 
 from cozy.ext import inject
 
-from gi.repository import Gtk, Adw
+from gi.repository import Adw
 
 
-@Gtk.Template(resource_path='/com/github/geigi/cozy/delete_book_dialog.ui')
 class DeleteBookView(Adw.MessageDialog):
-    __gtype_name__ = 'DeleteBookDialog'
-
     main_window = inject.attr("MainWindow")
 
     def __init__(self, callback, book):
-        super().__init__(modal=True, transient_for=self.main_window.window)
+        super().__init__(
+            heading=_("Delete Audiobook?"),
+            body=_("The audiobook will be removed from your disk and from Cozy's library."),
+            default_response="cancel",
+            close_response="cancel",
+            transient_for=self.main_window.window,
+            modal=True,
+        )
+
         self.add_response("cancel", _("Cancel"))
         self.add_response("delete", _("Remove Audiobook"))
         self.set_response_appearance("delete", Adw.ResponseAppearance.DESTRUCTIVE)
