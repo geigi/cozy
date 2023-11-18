311a92b00ac15ed1edd3465bb50eda25925cd851
rdbende <rdbende@proton.me>
2023-11-18 15:40:37 +0100

    Use AdwStatusPage in yet another place

--- cozy/ui/widgets/welcome.py
+++ cozy/ui/widgets/welcome.py
@@ -1,11 +1,10 @@
 import gi
 
-gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+from gi.repository import Adw, Gtk
 
 
 @Gtk.Template.from_resource('/com/github/geigi/cozy/welcome.ui')
-class Welcome(Gtk.Box):
+class Welcome(Adw.Bin):
     __gtype_name__ = "Welcome"
 
     def __init__(self, **kwargs):
