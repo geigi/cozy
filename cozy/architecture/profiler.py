bb9d1b6ae6f4d72fca59774ca200ed76644892be
naglis <827324+naglis@users.noreply.github.com>
2023-12-14 07:50:35 +0200

    Use `time.perf_counter()` for timing
    `time.time()` can return a lower value than a previous call if the
    system clock has been set back between the two calls.

--- cozy/architecture/profiler.py
+++ cozy/architecture/profiler.py
@@ -7,9 +7,9 @@ log = logging.getLogger("timing")
 def timing(f):
     @functools.wraps(f)
     def wrap(*args):
-        time1 = time.time()
+        time1 = time.perf_counter()
         ret = f(*args)
-        time2 = time.time()
+        time2 = time.perf_counter()
         log.info('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
 
         return ret
