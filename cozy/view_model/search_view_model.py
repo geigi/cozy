93d695a3e6e547fd0e617324a15929f35fba65cc
rdbende <rdbende@proton.me>
2024-01-06 12:15:43 +0100

    Refactor query of online books

--- cozy/view_model/search_view_model.py
+++ cozy/view_model/search_view_model.py
@@ -22,43 +22,33 @@ class SearchViewModel(Observable, EventSender):
         super().__init__()
         super(Observable, self).__init__()
 
-    @property
-    def authors(self):
+    def _get_available_books(self) -> set[Book]:
         is_book_online = self._fs_monitor.get_book_online
         show_offline_books = not self._application_settings.hide_offline
 
-        authors = {
-            book.author for book in self._model.books if is_book_online(book) or show_offline_books
+        return {
+            book for book in self._model.books if is_book_online(book) or show_offline_books
         }
-
-        return split_strings_to_set(authors)
-
-    @property
-    def readers(self):
-        is_book_online = self._fs_monitor.get_book_online
-        show_offline_books = not self._application_settings.hide_offline
-
-        readers = {
-            book.reader for book in self._model.books if is_book_online(book) or show_offline_books
-        }
-
-        return split_strings_to_set(readers)
 
     def search(
         self, search_query: str, callback: Callable[[list[Book], list[str], list[str]], None]
     ) -> None:
         search_query = search_query.lower()
 
+        available_books = self._get_available_books()
         books = {
             book
-            for book in self._model.books
+            for book in available_books
             if search_query in book.name.lower()
             or search_query in book.author.lower()
             or search_query in book.reader.lower()
         }
 
-        authors = {author for author in self.authors if search_query in author.lower()}
-        readers = {reader for reader in self.readers if search_query in reader.lower()}
+        available_book_authors = split_strings_to_set({book.author for book in available_books})
+        authors = {author for author in available_book_authors if search_query in author.lower()}
+
+        available_book_readers = split_strings_to_set({book.reader for book in available_books})
+        readers = {reader for reader in available_book_readers if search_query in reader.lower()}
 
         GLib.MainContext.default().invoke_full(
             GLib.PRIORITY_DEFAULT,
