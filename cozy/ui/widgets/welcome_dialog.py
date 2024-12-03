72155759b6144c3826d853bf148e9c9f653c0e28
Dévényi Benedek <rdbende@proton.me>
2024-12-03 19:51:50 +0100

    Finish up welcome dialog, correct some strings

--- cozy/ui/widgets/welcome_dialog.py
+++ cozy/ui/widgets/welcome_dialog.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import inject
+import os.path
 from gi.repository import Adw, Gtk
 
 from cozy.view_model.playback_speed_view_model import PlaybackSpeedViewModel
@@ -22,6 +23,7 @@ class WelcomeDialog(Adw.Dialog):
     reporting_page: Gtk.Box = Gtk.Template.Child()
     locations_page: Adw.StatusPage = Gtk.Template.Child()
     create_directory_switch: Adw.SwitchRow = Gtk.Template.Child()
+    chooser_button_label: Adw.ButtonContent = Gtk.Template.Child()
 
     def __init__(self):
         super().__init__()
@@ -29,17 +31,30 @@ class WelcomeDialog(Adw.Dialog):
         self._order = [self.welcome_page, self.reporting_page, self.locations_page]
 
     @Gtk.Template.Callback()
+    def advance(self, *_):
+        self.carousel.scroll_to(self._order[int(self.carousel.get_position()) + 1], True)
+
+    @Gtk.Template.Callback()
     def deny_reporting(self, _):
-        self.advance()
         self.app_settings.report_level = 0
+        self.advance()
 
     @Gtk.Template.Callback()
     def accept_reporting(self, _):
         self.advance()
 
     @Gtk.Template.Callback()
-    def done(self, _):
+    def choose_directory(self, _):
+        ask_storage_location(self._ask_storage_location_callback, None)
+
+    def _ask_storage_location_callback(self, path):
+        self._path = path
+        self.chooser_button_label.set_label(os.path.basename(path))
+
+    @Gtk.Template.Callback()
+    def done(self, __chooser_button_label):
         self.close()
+        self.app_settings.first_launch = False
 
         if self.create_directory_switch.props.active:
             audiobooks_dir = Path.home() / _("Audiobooks")
@@ -49,10 +64,3 @@ class WelcomeDialog(Adw.Dialog):
             inject.instance("MainWindow")._set_audiobook_path(self._path, default=False)
         else:
             inject.instance("MainWindow")._set_audiobook_path(self._path)
-
-    @Gtk.Template.Callback()
-    def choose_directory(self, _):
-        ask_storage_location(lambda path: setattr(self, "_path", path), None)
-
-    def advance(self):
-        self.carousel.scroll_to(self._order[int(self.carousel.get_position()) + 1], True)
